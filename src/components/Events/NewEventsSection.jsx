2f9ce60b751cfd4d0de9d29d5b61a85390cd6835
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-11-21 23:35:48 +0200

    added react query implementation

---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,45 @@
+import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import EventItem from "./EventItem.jsx";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from '../../util/http.js';
+
+export default function NewEventsSection() {
+  const { isLoading, error , isError, data} = useQuery({
+    queryFn: fetchEvents,
+    queryKey: ["events"],
+  });
+
+  let content;
+
+  if (isLoading) {
+    content = <LoadingIndicator />;
+  }
+
+  if (error) {
+    content = (
+      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className="content-section" id="new-events-section">
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
